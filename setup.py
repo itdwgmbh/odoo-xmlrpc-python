75725029443d22eb82f7c17cf17b9e3fde7ad235
Daniel Waldmann <25527351+dwaldmannDE@users.noreply.github.com>
2023-11-27 18:15:53 +0100

    added setup.py - first draft

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='odoo-xmlrpc',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'logging',
+        'xmlrpc'
+    ],
+    author='IT-DW GmbH',
+    author_email='support@it-dw.com',
+    description='A Python module to interact with Odoo XML-RPC API',
+    keywords='odoo xml-rpc api',
+    url='https://github.com/itdwgmbh/odoo-xmlrpc-python'
+)
