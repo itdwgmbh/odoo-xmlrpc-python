2ee81c46b6a0e8b57081286a4b269420f4087a14
Daniel Waldmann <25527351+dwaldmannDE@users.noreply.github.com>
2023-11-27 19:02:14 +0100

    added license

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='odoo_xmlrpc',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
@@ -10,5 +10,6 @@ setup(
     author_email='support@it-dw.com',
     description='A Python module to interact with Odoo XML-RPC API',
     keywords='odoo xml-rpc api',
-    url='https://github.com/itdwgmbh/odoo-xmlrpc-python'
+    url='https://github.com/itdwgmbh/odoo-xmlrpc-python',
+    license='GPLv3'
 )
